c786d86a9b4125eaabcbaf2c200b361d3cd634ae
VaheAA <vahe.abov@gmail.com>
2021-08-23 17:53:55 +0400

    cal

--- js/main.js
+++ js/main.js
@@ -12,23 +12,37 @@ const tip15Label = document.getElementById('tip-15-label');
 const tip25Label = document.getElementById('tip-25-label');
 const tip50Label = document.getElementById('tip-50-label');
 
-tip5.addEventListener('change', () => {
-    if(tip5.checked) {
+tip5.addEventListener('click', () => {
+    tip5.checked = true;
+    tip10.disabled = true;
+    tip15.disabled = true;
+    tip25.disabled = true;
+    tip50.disabled = true;
+
+    if (!tip5.checked) {
+        tip5.checked = false;
         tip5Label.classList.toggle('uncheked')
         tip5Label.classList.toggle('checked');
+        tip10.disabled = false;
+        tip15.disabled = false;
+        tip25.disabled = false;
+        tip50.disabled = false;
+    } else {
+        tip5Label.classList.toggle('uncheked')
+        tip5Label.classList.toggle('checked');
+
     }
-    console.log(tip5.dataset)
 })
 
 tip10.addEventListener('change', () => {
-    if(tip10.checked) {
+    if (tip10.checked) {
         tip10Label.classList.toggle('uncheked')
         tip10Label.classList.toggle('checked');
     }
 })
 
 tip15.addEventListener('change', () => {
-    if(tip15.checked) {
+    if (tip15.checked) {
         tip15Label.classList.toggle('uncheked')
         tip15Label.classList.toggle('checked');
     }
@@ -36,15 +50,15 @@ tip15.addEventListener('change', () => {
 })
 
 tip25.addEventListener('change', () => {
-    if(tip25.checked) {
+    if (tip25.checked) {
         tip25Label.classList.toggle('uncheked')
-        tip25Label.classList.toggle('checked'); 
+        tip25Label.classList.toggle('checked');
     }
 
 })
 
 tip50.addEventListener('change', () => {
-    if(tip50.checked) {
+    if (tip50.checked) {
         tip50Label.classList.toggle('uncheked')
         tip50Label.classList.toggle('checked');
     }
