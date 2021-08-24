e84837cb01a47065cd13aa8e8c2227bf550a0a09
Vahe Abovyan <VAbovyan@beeline.am>
2021-08-24 15:33:47 +0400

    updatik

--- js/main.js
+++ js/main.js
@@ -4,26 +4,36 @@ const numOfPeople = document.querySelector('#people');
 const sumTotal = document.querySelector('#sum-total');
 const sumPerson = document.querySelector("#sum-person");
 const resetBtn = document.querySelector('#reset-btn');
-
+const customTip = document.querySelector('#custom-tip')
 
 
 tipBtns.forEach(btn => {
     btn.addEventListener('click', (e) => {
+
+
+        e.currentTarget.classList.toggle('checked');
         const tipSize = e.currentTarget.getAttribute('id');
         const billAmount = bill.value;
         const people = numOfPeople.value;
         const tipFinal = (billAmount / 100) * tipSize;
-        sumTotal.textContent = tipFinal
-        sumPerson.textContent = tipFinal / people;
+        const personFinal = tipFinal / people
+        sumTotal.textContent = '$' + tipFinal.toFixed(2)
+        sumPerson.textContent = '$' + personFinal.toFixed(2);
         if (people === '' || people === 0) {
-            sumPerson.textContent = tipFinal
+            sumPerson.textContent = '$' + tipFinal.toFixed(2)
         }
+        resetBtn.classList.add('active-btn')
+
+
+        resetBtn.addEventListener('click', () => {
+            sumPerson.textContent = '$0.00';
+            sumTotal.textContent = '$0.00';
+            bill.value = '';
+            resetBtn.classList.remove('active-btn')
+            if (btn.classList.contains('checked')) {
+                btn.classList.remove('checked');
+            }
+        })
     })
 })
 
-resetBtn.addEventListener('click', () => {
-    sumPerson.textContent = '$0.00'
-    sumTotal.textContent = '$0.00'
-    bill.value = ''
-
-})
