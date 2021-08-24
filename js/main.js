cd6194816e87d1511d505b8a472e5b3fcc809db2
Vahe Abovyan <VAbovyan@beeline.am>
2021-08-24 12:18:11 +0400

    new calculation functionalit

--- js/main.js
+++ js/main.js
@@ -1,66 +1,29 @@
+const tipBtns = document.querySelectorAll('.tip-control');
+const bill = document.getElementById('bill');
+const numOfPeople = document.querySelector('#people');
+const sumTotal = document.querySelector('#sum-total');
+const sumPerson = document.querySelector("#sum-person");
+const resetBtn = document.querySelector('#reset-btn');
 
-const tip5 = document.getElementById('tip-5');
-const tip10 = document.getElementById('tip-10');
-const tip15 = document.getElementById('tip-15');
-const tip25 = document.getElementById('tip-25');
-const tip50 = document.getElementById('tip-50');
 
 
-const tip5Label = document.getElementById('tip-5-label');
-const tip10Label = document.getElementById('tip-10-label');
-const tip15Label = document.getElementById('tip-15-label');
-const tip25Label = document.getElementById('tip-25-label');
-const tip50Label = document.getElementById('tip-50-label');
-
-tip5.addEventListener('click', () => {
-    tip5.checked = true;
-    tip10.disabled = true;
-    tip15.disabled = true;
-    tip25.disabled = true;
-    tip50.disabled = true;
-
-    if (!tip5.checked) {
-        tip5.checked = false;
-        tip5Label.classList.toggle('uncheked')
-        tip5Label.classList.toggle('checked');
-        tip10.disabled = false;
-        tip15.disabled = false;
-        tip25.disabled = false;
-        tip50.disabled = false;
-    } else {
-        tip5Label.classList.toggle('uncheked')
-        tip5Label.classList.toggle('checked');
-
-    }
+tipBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const tipSize = e.currentTarget.getAttribute('id');
+        const billAmount = bill.value;
+        const people = numOfPeople.value;
+        const tipFinal = (billAmount / 100) * tipSize;
+        sumTotal.textContent = tipFinal
+        sumPerson.textContent = tipFinal / people;
+        if (people === '' || people === 0) {
+            sumPerson.textContent = tipFinal
+        }
+    })
 })
 
-tip10.addEventListener('change', () => {
-    if (tip10.checked) {
-        tip10Label.classList.toggle('uncheked')
-        tip10Label.classList.toggle('checked');
-    }
-})
-
-tip15.addEventListener('change', () => {
-    if (tip15.checked) {
-        tip15Label.classList.toggle('uncheked')
-        tip15Label.classList.toggle('checked');
-    }
-
-})
-
-tip25.addEventListener('change', () => {
-    if (tip25.checked) {
-        tip25Label.classList.toggle('uncheked')
-        tip25Label.classList.toggle('checked');
-    }
-
-})
-
-tip50.addEventListener('change', () => {
-    if (tip50.checked) {
-        tip50Label.classList.toggle('uncheked')
-        tip50Label.classList.toggle('checked');
-    }
+resetBtn.addEventListener('click', () => {
+    sumPerson.textContent = '$0.00'
+    sumTotal.textContent = '$0.00'
+    bill.value = ''
 
 })
